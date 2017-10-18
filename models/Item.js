9ce5d0126536233c3dffeea8329d33ebbfa0090d
Ryan Jarnutowski <rjarnutowski@bluestatedigital.com>
2017-10-18 15:38:57 -0400

    fixing

--- models/Item.js
+++ models/Item.js
@@ -3,14 +3,14 @@ var Schema = mongoose.Schema;
 
 // Define collection and schema for Items
 var Item = new Schema({
-  name: {
-    type: String
-  },
-  price: {
-    type: Number
-  }
-},{
-	collection: 'items'
+    name: {
+        type: String
+    },
+    price: {
+        type: Number
+    }
+}, {
+    collection: 'items'
 });
 
 module.exports = mongoose.model('Item', Item);
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-var express = require('express'),
-      path = require('path'),
-      bodyParser = require('body-parser'),
-      cors = require('cors'),
-      mongoose = require('mongoose');
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    mongoose = require('mongoose'),
+    config = require('./config/DB'),
+    itemRoutes = require('./expressRoutes/itemRoutes');
 
-      const app = express();
-      var port = process.env.PORT || 4000;
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB).then(
+    () => {
+        console.log('Database is connected')
+    },
+    err => {
+        console.log('Can not connect to the database' + err)
+    }
+);
 
-      var server = app.listen(function(){
-        console.log('Listening on port ' + port);
-      });
-      
+const app = express();
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/items', itemRoutes);
+const port = process.env.PORT || 4000;
+
+const server = app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
